5ad8c87dc4a95e6ae253d857b6f3036bbe8c77b9
Kim Seonghyeon <kim.seonghyeon@snu.ac.kr>
2019-12-16 06:56:52 +0000

    Added NVIDIA license

--- op/upfirdn2d_kernel.cu
+++ op/upfirdn2d_kernel.cu
@@ -1,3 +1,9 @@
+// Copyright (c) 2019, NVIDIA Corporation. All rights reserved.
+//
+// This work is made available under the Nvidia Source Code License-NC.
+// To view a copy of this license, visit
+// https://nvlabs.github.io/stylegan2/license.html
+
 #include <torch/types.h>
 
 #include <ATen/ATen.h>
