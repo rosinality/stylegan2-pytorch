05cd16d70fd02fced79e8e96b4940765415c6385
Terence Broad <terry.m.broad@gmail.com>
2020-03-05 16:31:49 +0000

    add truncation trick to generate.pu

--- generate.py
+++ generate.py
@@ -4,14 +4,14 @@ import torch
 from torchvision import utils
 from model import Generator
 from tqdm import tqdm
-def generate(args, g_ema, device):
+def generate(args, g_ema, device, mean_latent):
 
     with torch.no_grad():
         g_ema.eval()
         for i in tqdm(range(args.pics)):
            sample_z = torch.randn(args.sample, args.latent, device=device)
 
-           sample, _ = g_ema([sample_z])
+           sample, _ = g_ema([sample_z], truncation=args.truncation, truncation_latent=mean_latent)
            
            utils.save_image(
             sample,
@@ -29,7 +29,8 @@ if __name__ == '__main__':
     parser.add_argument('--size', type=int, default=1024)
     parser.add_argument('--sample', type=int, default=1)
     parser.add_argument('--pics', type=int, default=20)
-
+    parser.add_argument('--truncation', type=float, default=1)
+    parser.add_argument('--truncation_mean', type=int, default=4096)
     parser.add_argument('--ckpt', type=str, default="stylegan2-ffhq-config-f.pt")
     parser.add_argument('--channel_multiplier', type=int, default=2)
 
@@ -45,6 +46,10 @@ if __name__ == '__main__':
 
     g_ema.load_state_dict(checkpoint['g_ema'])
 
+    if args.truncation < 1:
+        with torch.no_grad():
+            mean_latent = g_ema.mean_latent(args.truncation_mean)
+    else:
+        mean_latent = None
 
-
-    generate(args, g_ema, device)
+    generate(args, g_ema, device, mean_latent)
