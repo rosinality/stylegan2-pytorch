27e6c994c1a3ded0e5c30a8746473db67a5566ce
Kim Seonghyeon <kim.seonghyeon@snu.ac.kr>
2020-06-07 23:13:23 +0000

    Set map_location

--- train.py
+++ train.py
@@ -145,7 +145,7 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
     else:
         g_module = generator
         d_module = discriminator
-        
+
     accum = 0.5 ** (32 / (10 * 1000))
 
     sample_z = torch.randn(args.n_sample, args.latent, device=device)
@@ -154,7 +154,7 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
         i = idx + args.start_iter
 
         if i > args.iter:
-            print('Done!')
+            print("Done!")
 
             break
 
@@ -171,9 +171,9 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
         real_pred = discriminator(real_img)
         d_loss = d_logistic_loss(real_pred, fake_pred)
 
-        loss_dict['d'] = d_loss
-        loss_dict['real_score'] = real_pred.mean()
-        loss_dict['fake_score'] = fake_pred.mean()
+        loss_dict["d"] = d_loss
+        loss_dict["real_score"] = real_pred.mean()
+        loss_dict["fake_score"] = fake_pred.mean()
 
         discriminator.zero_grad()
         d_loss.backward()
@@ -191,7 +191,7 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
 
             d_optim.step()
 
-        loss_dict['r1'] = r1_loss
+        loss_dict["r1"] = r1_loss
 
         requires_grad(generator, True)
         requires_grad(discriminator, False)
@@ -201,7 +201,7 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
         fake_pred = discriminator(fake_img)
         g_loss = g_nonsaturating_loss(fake_pred)
 
-        loss_dict['g'] = g_loss
+        loss_dict["g"] = g_loss
 
         generator.zero_grad()
         g_loss.backward()
@@ -211,9 +211,7 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
 
         if g_regularize:
             path_batch_size = max(1, args.batch // args.path_batch_shrink)
-            noise = mixing_noise(
-                path_batch_size, args.latent, args.mixing, device
-            )
+            noise = mixing_noise(path_batch_size, args.latent, args.mixing, device)
             fake_img, latents = generator(noise, return_latents=True)
 
             path_loss, mean_path_length, path_lengths = g_path_regularize(
@@ -234,40 +232,40 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
                 reduce_sum(mean_path_length).item() / get_world_size()
             )
 
-        loss_dict['path'] = path_loss
-        loss_dict['path_length'] = path_lengths.mean()
+        loss_dict["path"] = path_loss
+        loss_dict["path_length"] = path_lengths.mean()
 
         accumulate(g_ema, g_module, accum)
 
         loss_reduced = reduce_loss_dict(loss_dict)
 
-        d_loss_val = loss_reduced['d'].mean().item()
-        g_loss_val = loss_reduced['g'].mean().item()
-        r1_val = loss_reduced['r1'].mean().item()
-        path_loss_val = loss_reduced['path'].mean().item()
-        real_score_val = loss_reduced['real_score'].mean().item()
-        fake_score_val = loss_reduced['fake_score'].mean().item()
-        path_length_val = loss_reduced['path_length'].mean().item()
+        d_loss_val = loss_reduced["d"].mean().item()
+        g_loss_val = loss_reduced["g"].mean().item()
+        r1_val = loss_reduced["r1"].mean().item()
+        path_loss_val = loss_reduced["path"].mean().item()
+        real_score_val = loss_reduced["real_score"].mean().item()
+        fake_score_val = loss_reduced["fake_score"].mean().item()
+        path_length_val = loss_reduced["path_length"].mean().item()
 
         if get_rank() == 0:
             pbar.set_description(
                 (
-                    f'd: {d_loss_val:.4f}; g: {g_loss_val:.4f}; r1: {r1_val:.4f}; '
-                    f'path: {path_loss_val:.4f}; mean path: {mean_path_length_avg:.4f}'
+                    f"d: {d_loss_val:.4f}; g: {g_loss_val:.4f}; r1: {r1_val:.4f}; "
+                    f"path: {path_loss_val:.4f}; mean path: {mean_path_length_avg:.4f}"
                 )
             )
 
             if wandb and args.wandb:
                 wandb.log(
                     {
-                        'Generator': g_loss_val,
-                        'Discriminator': d_loss_val,
-                        'R1': r1_val,
-                        'Path Length Regularization': path_loss_val,
-                        'Mean Path Length': mean_path_length,
-                        'Real Score': real_score_val,
-                        'Fake Score': fake_score_val,
-                        'Path Length': path_length_val,
+                        "Generator": g_loss_val,
+                        "Discriminator": d_loss_val,
+                        "R1": r1_val,
+                        "Path Length Regularization": path_loss_val,
+                        "Mean Path Length": mean_path_length,
+                        "Real Score": real_score_val,
+                        "Fake Score": fake_score_val,
+                        "Path Length": path_length_val,
                     }
                 )
 
@@ -277,7 +275,7 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
                     sample, _ = g_ema([sample_z])
                     utils.save_image(
                         sample,
-                        f'sample/{str(i).zfill(6)}.png',
+                        f"sample/{str(i).zfill(6)}.png",
                         nrow=int(args.n_sample ** 0.5),
                         normalize=True,
                         range=(-1, 1),
@@ -286,46 +284,46 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
             if i % 10000 == 0:
                 torch.save(
                     {
-                        'g': g_module.state_dict(),
-                        'd': d_module.state_dict(),
-                        'g_ema': g_ema.state_dict(),
-                        'g_optim': g_optim.state_dict(),
-                        'd_optim': d_optim.state_dict(),
+                        "g": g_module.state_dict(),
+                        "d": d_module.state_dict(),
+                        "g_ema": g_ema.state_dict(),
+                        "g_optim": g_optim.state_dict(),
+                        "d_optim": d_optim.state_dict(),
                     },
-                    f'checkpoint/{str(i).zfill(6)}.pt',
+                    f"checkpoint/{str(i).zfill(6)}.pt",
                 )
 
 
-if __name__ == '__main__':
-    device = 'cuda'
+if __name__ == "__main__":
+    device = "cuda"
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('path', type=str)
-    parser.add_argument('--iter', type=int, default=800000)
-    parser.add_argument('--batch', type=int, default=16)
-    parser.add_argument('--n_sample', type=int, default=64)
-    parser.add_argument('--size', type=int, default=256)
-    parser.add_argument('--r1', type=float, default=10)
-    parser.add_argument('--path_regularize', type=float, default=2)
-    parser.add_argument('--path_batch_shrink', type=int, default=2)
-    parser.add_argument('--d_reg_every', type=int, default=16)
-    parser.add_argument('--g_reg_every', type=int, default=4)
-    parser.add_argument('--mixing', type=float, default=0.9)
-    parser.add_argument('--ckpt', type=str, default=None)
-    parser.add_argument('--lr', type=float, default=0.002)
-    parser.add_argument('--channel_multiplier', type=int, default=2)
-    parser.add_argument('--wandb', action='store_true')
-    parser.add_argument('--local_rank', type=int, default=0)
+    parser.add_argument("path", type=str)
+    parser.add_argument("--iter", type=int, default=800000)
+    parser.add_argument("--batch", type=int, default=16)
+    parser.add_argument("--n_sample", type=int, default=64)
+    parser.add_argument("--size", type=int, default=256)
+    parser.add_argument("--r1", type=float, default=10)
+    parser.add_argument("--path_regularize", type=float, default=2)
+    parser.add_argument("--path_batch_shrink", type=int, default=2)
+    parser.add_argument("--d_reg_every", type=int, default=16)
+    parser.add_argument("--g_reg_every", type=int, default=4)
+    parser.add_argument("--mixing", type=float, default=0.9)
+    parser.add_argument("--ckpt", type=str, default=None)
+    parser.add_argument("--lr", type=float, default=0.002)
+    parser.add_argument("--channel_multiplier", type=int, default=2)
+    parser.add_argument("--wandb", action="store_true")
+    parser.add_argument("--local_rank", type=int, default=0)
 
     args = parser.parse_args()
 
-    n_gpu = int(os.environ['WORLD_SIZE']) if 'WORLD_SIZE' in os.environ else 1
+    n_gpu = int(os.environ["WORLD_SIZE"]) if "WORLD_SIZE" in os.environ else 1
     args.distributed = n_gpu > 1
 
     if args.distributed:
         torch.cuda.set_device(args.local_rank)
-        torch.distributed.init_process_group(backend='nccl', init_method='env://')
+        torch.distributed.init_process_group(backend="nccl", init_method="env://")
         synchronize()
 
     args.latent = 512
@@ -360,23 +358,23 @@ if __name__ == '__main__':
     )
 
     if args.ckpt is not None:
-        print('load model:', args.ckpt)
-        
-        ckpt = torch.load(args.ckpt)
+        print("load model:", args.ckpt)
+
+        ckpt = torch.load(args.ckpt, map_location=lambda storage, loc: storage)
 
         try:
             ckpt_name = os.path.basename(args.ckpt)
             args.start_iter = int(os.path.splitext(ckpt_name)[0])
-            
+
         except ValueError:
             pass
-            
-        generator.load_state_dict(ckpt['g'])
-        discriminator.load_state_dict(ckpt['d'])
-        g_ema.load_state_dict(ckpt['g_ema'])
 
-        g_optim.load_state_dict(ckpt['g_optim'])
-        d_optim.load_state_dict(ckpt['d_optim'])
+        generator.load_state_dict(ckpt["g"])
+        discriminator.load_state_dict(ckpt["d"])
+        g_ema.load_state_dict(ckpt["g_ema"])
+
+        g_optim.load_state_dict(ckpt["g_optim"])
+        d_optim.load_state_dict(ckpt["d_optim"])
 
     if args.distributed:
         generator = nn.parallel.DistributedDataParallel(
@@ -392,7 +390,6 @@ if __name__ == '__main__':
             output_device=args.local_rank,
             broadcast_buffers=False,
         )
-
 
     transform = transforms.Compose(
         [
@@ -411,6 +408,6 @@ if __name__ == '__main__':
     )
 
     if get_rank() == 0 and wandb is not None and args.wandb:
-        wandb.init(project='stylegan 2')
+        wandb.init(project="stylegan 2")
 
     train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, device)
