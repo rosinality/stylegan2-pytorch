c9ad1bef13de27fb56393f3f4fedf62cd2ae2d08
Wok <woctezuma@users.noreply.github.com>
2020-09-19 15:45:45 +0200

    Save image as JPG instead of PNG

--- apply_factor.py
+++ apply_factor.py
@@ -87,7 +87,7 @@ if __name__ == "__main__":
 
     grid = utils.save_image(
         torch.cat([img1, img, img2], 0),
-        f"{args.out_prefix}_index-{args.index}_degree-{args.degree}.png",
+        f"{args.out_prefix}_index-{args.index}_degree-{args.degree}.jpg",
         normalize=True,
         range=(-1, 1),
         nrow=args.n_sample,
