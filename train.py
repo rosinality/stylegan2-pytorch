f7f3859f11c6ee989d8a0a85f56e1ea2646f3d61
Amit Abecasis <amitabe@weizmann.ac.il>
2022-03-03 10:48:15 +0200

    Refactor save_image argument
    use range_value argument instead of range argument as it's being
    deprecated

--- train.py
+++ train.py
@@ -311,7 +311,7 @@ def train(args, loader, generator, discriminator, g_optim, d_optim, g_ema, devic
                         f"sample/{str(i).zfill(6)}.png",
                         nrow=int(args.n_sample ** 0.5),
                         normalize=True,
-                        range=(-1, 1),
+                        range_value=(-1, 1),
                     )
 
             if i % 10000 == 0:
